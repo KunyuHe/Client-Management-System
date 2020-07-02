9e26b88bfb242d04122eca9d34f012580331c3bc
Kunyu He <kunyuhe@uchicago.edu>
2020-07-02 02:37:51 -0500

    fix: add unique constraint for username and client name

--- backend/app/models/model.py
+++ backend/app/models/model.py
@@ -5,41 +5,40 @@ from app.utils.core import db
 # 用户-客户多对多
 user_client = db.Table('user_client',
                        db.Column('user_id', db.Integer,
-                                 db.ForeignKey('user.id'),
+                                 db.ForeignKey('users.id'),
                                  primary_key=True),
                        db.Column('client_id', db.Integer,
-                                 db.ForeignKey('client.id'),
+                                 db.ForeignKey('clients.id'),
                                  primary_key=True))
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)  # 用户名
-    password = db.Column(db.String(255), nullable=False)
-    phone = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     clients = db.relationship('Client', secondary=user_client,
                               cascade="all,delete", backref="users")
 
 
 class Client(db.Model):
-    __tablename__ = "client"
+    __tablename__ = "clients"
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False)
 
     incomes = db.relationship('Income', cascade="all,delete", backref="client")
 
 
 class Income(db.Model):
-    __tablename__ = "income"
+    __tablename__ = "incomes"
 
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"))
+    client_id = db.Column(db.Integer, db.ForeignKey("clients.id"))
     value = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date,
                      default=(datetime.utcnow() + timedelta(hours=8)).strftime(
